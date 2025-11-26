e3fa30490e1dd9d74ca7be3242fc82172749e749
hiykoe <21hoang.01hieu@gmail.com>
2025-11-26 21:00:50 +0700

    Update admin_users.php

--- admin_users.php
+++ admin_users.php
@@ -9,6 +9,26 @@ $admin_id = $_SESSION['admin_id'];
 if(!isset($admin_id)){
    header('location:login.php');
 }
+if(isset($_POST['update_user'])){
+   $id = $_POST['update_id'];
+   $name = $_POST['name'];
+   $email = $_POST['email'];
+   $user_type = $_POST['user_type'];
+   $password = $_POST['password'];
+
+   if(!empty($password)){
+      $password = md5($password);
+      mysqli_query($conn, "UPDATE `users` 
+         SET name='$name', email='$email', user_type='$user_type', password='$password'
+         WHERE id='$id'") or die('query failed');
+   } else {
+      mysqli_query($conn, "UPDATE `users` 
+         SET name='$name', email='$email', user_type='$user_type'
+         WHERE id='$id'") or die('query failed');
+   }
+
+   header('location:admin_users.php');
+}
 
 if(isset($_GET['delete'])){
    $delete_id = $_GET['delete'];
@@ -57,6 +77,39 @@ if(isset($_GET['delete'])){
 <a href="admin_users.php?edit=<?php echo $fetch_users['id']; ?>" class="option-btn">chỉnh sửa</a>
          <a href="admin_users.php?delete=<?php echo $fetch_users['id']; ?>
             " onclick="return confirm('delete this user?');" class="delete-btn">xóa người dùng</a>
+         <?php
+if(isset($_GET['edit'])){
+   $edit_id = $_GET['edit'];
+   $select_edit = mysqli_query($conn, "SELECT * FROM `users` WHERE id = '$edit_id'") or die('query failed');
+   $row = mysqli_fetch_assoc($select_edit);
+?>
+<div class="box update-box">
+   <h3>Chỉnh sửa người dùng</h3>
+
+   <form action="" method="post">
+      <input type="hidden" name="update_id" value="<?php echo $row['id']; ?>">
+
+      <p>Tên người dùng</p>
+      <input type="text" name="name" value="<?php echo $row['name']; ?>" required>
+
+      <p>Email</p>
+      <input type="email" name="email" value="<?php echo $row['email']; ?>" required>
+
+      <p>Mật khẩu mới (để trống nếu không đổi)</p>
+      <input type="password" name="password">
+
+      <p>Loại người dùng</p>
+      <select name="user_type">
+         <option value="user" <?php if($row['user_type']=='user') echo 'selected'; ?>>user</option>
+         <option value="admin" <?php if($row['user_type']=='admin') echo 'selected'; ?>>admin</option>
+      </select>
+
+      <input type="submit" name="update_user" value="Cập nhật" class="btn">
+      <a href="admin_users.php" class="option-btn">Hủy</a>
+   </form>
+</div>
+<?php } ?>
+
       </div>
 
       <?php
