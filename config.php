b08ac7d6b5b845fe0accf2cee37a8efe5692f44d
toilagatau234 <156187898+toilagatau234@users.noreply.github.com>
2025-12-17 17:48:39 +0700

    Change database name format in config.php

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'bookshop-website');
+define('DB_NAME', 'bookshop_website');
 define('DB_PORT', '3306');
 ?>
